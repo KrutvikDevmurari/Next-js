7e4cdbdfc4f1c7dbcbb7b577c47ef910a9ea34ef
Krutvik Devmuari <krutvikdevmurari.vision@gmail.com>
2023-09-14 18:37:09 +0530

    database-setup

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Button from '../../components/UI/Button'
+
+const page = () => {
+    return (
+        <Button>Hello</Button>
+    )
+}
+
+export default page
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
+import connectDB from "@/lib/dbConnection"
 
-export default function Home() {
+export default async function Home() {
+  await connectDB().then(res => {
+    console.log("Databse connected Sucessfully");
+  }).catch(err => {
+    console.log(err)
+  })
   return (
-    <></>
+    <> hii</>
   )
 }
